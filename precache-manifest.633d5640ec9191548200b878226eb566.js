9d329d52290e679a76850d81e9765a81f6edcc2e
choihoonam <choossy23@gmail.com>
2019-10-18 10:42:13 +0900

    Updates

---
+++ precache-manifest.633d5640ec9191548200b878226eb566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31651b421f98bd84120859f09ef90c9a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "542829dd445c292ef0a8",
+    "url": "/movie_app/static/css/main.58a367ab.chunk.css"
+  },
+  {
+    "revision": "54662ea1af6317c46be7",
+    "url": "/movie_app/static/js/2.d2a361f7.chunk.js"
+  },
+  {
+    "revision": "542829dd445c292ef0a8",
+    "url": "/movie_app/static/js/main.33810715.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
